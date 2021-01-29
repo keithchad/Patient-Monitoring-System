8d557fa8b36e562f09d6d87ed27155c5eb11ccea
Chad <keithchad10@outlook.com>
2021-01-29 17:29:35 +0300

    editing EditProfileActivityDoctor

--- app/src/main/java/com/robo101/patientmonitoringsystem/activity/doctor/EditProfileActivityDoctor.java
+++ app/src/main/java/com/robo101/patientmonitoringsystem/activity/doctor/EditProfileActivityDoctor.java
@@ -1,43 +1,58 @@
 package com.robo101.patientmonitoringsystem.activity.doctor;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 import com.robo101.patientmonitoringsystem.R;
-import com.robo101.patientmonitoringsystem.model.User_Patient;
+import com.robo101.patientmonitoringsystem.activity.patient.MainActivityPatient;
+import com.robo101.patientmonitoringsystem.constants.Constants;
+import com.robo101.patientmonitoringsystem.model.User_Doctor;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class EditProfileActivityDoctor extends AppCompatActivity {
 
-    private ImageView imageClose;
     private ImageView imageProfile;
 
     private TextView textSave;
-    private TextView textChangePhoto;
 
     private ProgressBar progressBar;
 
-    private TextInputEditText edittextBirthdayEditProfile;
+    private TextInputEditText edittextSpecializationEditProfile;
     private TextInputEditText edittextFullname;
-    private TextInputEditText edittextAgeEditProfile;
+    private TextInputEditText edittextHospitalEditProfile;
 
     private FirebaseUser firebaseUser;
     private StorageReference storageReference;
-    private UploadTask uploadTask;
     private Uri imageUri;
 
     @Override
@@ -48,49 +63,62 @@ public class EditProfileActivityDoctor extends AppCompatActivity {
     }
 
     private void initialize() {
-        imageClose = findViewById(R.id.imageClose);
-        imageProfile = findViewById(R.id.imageProfileEditProfile);
+
+        ImageView imageClose = findViewById(R.id.imageClose);
+        imageProfile = findViewById(R.id.imageProfileEditProfileDoctor);
         textSave = findViewById(R.id.textSave);
-        textChangePhoto = findViewById(R.id.textChange);
-        edittextFullname = findViewById(R.id.edittextFullNameEditProfile);
-        edittextAgeEditProfile = findViewById(R.id.edittextAgeEditProfile);
-        edittextBirthdayEditProfile = findViewById(R.id.edittextBirthdayEditProfile);
+        TextView textChangePhoto = findViewById(R.id.textChangeDoctor);
+        edittextFullname = findViewById(R.id.edittextFullNameEditProfileDoctor);
+        edittextHospitalEditProfile = findViewById(R.id.edittextHospitalEditProfile);
+        edittextSpecializationEditProfile = findViewById(R.id.edittextSpecializationEditProfile);
         progressBar = findViewById(R.id.editProfileProgressBar);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        storageReference = FirebaseStorage.getInstance().getReference("Uploads");
+        storageReference = FirebaseStorage.getInstance().getReference("Doctor's Uploads");
 
         imageClose.setOnClickListener(v -> finish());
 
         textChangePhoto.setOnClickListener(v -> CropImage.activity()
                 .setAspectRatio(1,1)
                 .setCropShape(CropImageView.CropShape.OVAL)
-                .start(EditProfileActivity.this));
+                .start(EditProfileActivityDoctor.this));
 
         imageProfile.setOnClickListener(v -> CropImage.activity()
                 .setAspectRatio(1,1)
                 .setCropShape(CropImageView.CropShape.OVAL)
-                .start(EditProfileActivity.this));
+                .start(EditProfileActivityDoctor.this));
 
-        textSave.setOnClickListener(v ->
-                updateProfile(edittextFullname.getText().toString(),
-                        edittextBirthdayEditProfile.getText().toString(),
-                        edittextAgeEditProfile.getText().toString()));
+        textSave.setOnClickListener(v -> {
+            if(Objects.requireNonNull(edittextFullname.getText()).toString().trim().isEmpty()) {
+                Toast.makeText(EditProfileActivityDoctor.this, "Update Name", Toast.LENGTH_SHORT).show();
+                return;
+            }else if(Objects.requireNonNull(edittextHospitalEditProfile.getText()).toString().trim().isEmpty()) {
+                Toast.makeText(EditProfileActivityDoctor.this, "Update Hospital", Toast.LENGTH_SHORT).show();
+                return;
+            }else if(Objects.requireNonNull(edittextSpecializationEditProfile.getText()).toString().trim().isEmpty()) {
+                Toast.makeText(EditProfileActivityDoctor.this, "Update Specialization", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            updateProfile(edittextFullname.getText().toString(),
+                    edittextHospitalEditProfile.getText().toString(),
+                    edittextSpecializationEditProfile.getText().toString());
+        });
+
         getUserInfo();
     }
 
     private void getUserInfo() {
 
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users").child(firebaseUser.getUid());
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Doctors").child(firebaseUser.getUid());
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User_Patient userPatient = snapshot.getValue(User_Patient.class);
-                if (userPatient != null) {
-                    edittextFullname.setText(userPatient.getName());
-                    edittextBirthdayEditProfile.setText(userPatient.getBirthday());
-                    edittextAgeEditProfile.setText(userPatient.getAge());
-                    Glide.with(EditProfileActivity.this).load(userPatient.getImageUrl()).into(imageProfile);
+                User_Doctor userDoctor = snapshot.getValue(User_Doctor.class);
+                if (userDoctor != null) {
+                    edittextFullname.setText(userDoctor.getName());
+                    edittextSpecializationEditProfile.setText(userDoctor.getSpecialization());
+                    edittextHospitalEditProfile.setText(userDoctor.getHospital());
+                    Glide.with(EditProfileActivityDoctor.this).load(userDoctor.getImageUrl()).into(imageProfile);
                 }
             }
 
@@ -101,13 +129,13 @@ public class EditProfileActivityDoctor extends AppCompatActivity {
         });
     }
 
-    private void updateProfile(String fullname, String birthday, String age) {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users").child(firebaseUser.getUid());
+    private void updateProfile(String fullname, String hospital, String specialization) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Doctors").child(firebaseUser.getUid());
 
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put(Constants.NAME, fullname);
-        hashMap.put(Constants.BIRTHDAY, birthday);
-        hashMap.put(Constants.AGE, age);
+        hashMap.put(Constants.HOSPITAL, hospital);
+        hashMap.put(Constants.SPECIALIZATION, specialization);
 
         reference.updateChildren(hashMap);
     }
@@ -137,7 +165,7 @@ public class EditProfileActivityDoctor extends AppCompatActivity {
                     String url = downloadUri.toString();
 
                     DatabaseReference reference = FirebaseDatabase.getInstance()
-                            .getReference("Users").child(firebaseUser.getUid());
+                            .getReference("Doctors").child(firebaseUser.getUid());
 
                     HashMap<String, Object> hashMap = new HashMap<>();
                     hashMap.put(Constants.IMAGE_URL, ""+url);
@@ -147,7 +175,7 @@ public class EditProfileActivityDoctor extends AppCompatActivity {
                     progressBar.setVisibility(View.GONE);
                     textSave.setVisibility(View.VISIBLE);
 
-                    startActivity(new Intent(EditProfileActivity.this, MainActivityPatient.class));
+                    startActivity(new Intent(EditProfileActivityDoctor.this, MainActivityPatient.class));
                     finish();
                 } else {
                     progressBar.setVisibility(View.GONE);
